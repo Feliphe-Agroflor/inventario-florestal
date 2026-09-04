123053a25ad9997d56de93d3b138bd99012c8eec
Feliphe Rossi <felipe.rossit@agroflor.com.br>
2026-09-04 13:07:41 -0300

    deploy: adicionar campo tamanho da parcela para sub-parcelas Arbustivo/Herbaceo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "59828470" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1889119785" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
